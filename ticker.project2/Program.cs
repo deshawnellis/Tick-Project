b1295cc9a0212cd77bbf49c107f26aca932f16a7
deshawnellis <razzadiversa@gmail.com>
2022-03-15 20:21:37 -0400

    intial commit

---
+++ ticker.project2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ticker.project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This application will give you your standard stock information based off the ticker symbol");
+
+            Console.WriteLine("Enter the ticker symbol:");
+
+            string tickerSymbol = Console.ReadLine();
+            tickerSymbol = tickerSymbol;
+
+        }
+    }
+}
